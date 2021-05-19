36be2e96a35dd6e31184742e43adbda0d820fedf
Carlos Andrade <krlosgilson@gmail.com>
2021-05-19 14:40:12 -0300

    [dashboard] created the roles and validations

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,4 +1,28 @@
 class DashboardsController < ApplicationController
   def index
+    #@patients_attended_count = @appointments_realized.pluck(:patient_id).uniq.count
   end
+
+  def get_appointments_realized_by_doctor
+    appointments = appointments_realized params[:doctor_id]
+
+    render json: appointments.to_json
+  end
+
+  def get_patients_attended_count_by_doctor
+    appointments = appointments_realized params[:doctor_id]
+    patients_attended_count = appointments.pluck(:patient_id).uniq.count
+
+    render json: patients_attended_count.to_json
+  end
+
+  private
+
+    def appointments_realized doctor_id=nil
+      if doctor_id.present?
+        appointments_realized = Appointment.where('doctor_id = ? and ends_at <= ?', doctor_id, Time.new)
+      else
+        appointments_realized = Appointment.where('ends_at <= ?', Time.new)
+      end
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   root :to => "dashboards#index"
+
   match '/dashboard' => 'dashboards#index', via: [:get]
+  match '/get_appointments_realized_by_doctor' => 
+        'dashboards#get_appointments_realized_by_doctor', via: [:get]
+  match '/get_patients_attended_count_by_doctor' => 
+        'dashboards#get_patients_attended_count_by_doctor', via: [:get]
 
   resources :appointments
   match '/get_appointment_allowed_times_availables' =>
